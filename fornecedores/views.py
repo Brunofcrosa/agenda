b2350bbbce77690320b63b22e043e99c3471a381
Bruno Fetzer <brunofcrosa@hotmail.com>
2025-04-21 20:47:01 -0300

    Django-part5

--- fornecedores/views.py
+++ fornecedores/views.py
@@ -1,6 +1,8 @@
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
-
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
+from django.core.paginator import Paginator
 from .forms import FornecedorModelForm
 from .models import Fornecedor
 
@@ -12,23 +14,34 @@ class FornecedoresView(ListView):
     def get_queryset(self):
         buscar = self.request.GET.get('buscar')
         qs = super(FornecedoresView, self).get_queryset()
+
         if buscar:
-            return qs.filter(nome__icontains=buscar)
-        return qs
+            qs = qs.filter(nome__icontains=buscar)
+        
+        if qs.count() > 0:
+            paginator = Paginator(qs, 1)
+            listagem = paginator.get_page(self.request.GET.get('page'))
+            return listagem
+        else:
+            return messages.info(self.request, 'Não existem fornecedores cadastrados!')
+        
     
-class FornecedorAddView(CreateView):
+class FornecedorAddView(SuccessMessageMixin, CreateView):
     model = Fornecedor
     form_class = FornecedorModelForm
     template_name = 'fornecedor_form.html'
     success_url = reverse_lazy('fornecedores')
+    success_message = 'Fornecedor cadastrado com sucesso!'
 
-class FornecedorUpdateView(UpdateView):
+class FornecedorUpdateView(SuccessMessageMixin, UpdateView):
     model = Fornecedor
     form_class = FornecedorModelForm
     template_name = 'fornecedor_form.html'
     success_url = reverse_lazy('fornecedores')
+    success_message = 'Fornecedor alterado com sucesso!'
 
-class FornecedorDeleteView(DeleteView):
+class FornecedorDeleteView(SuccessMessageMixin, DeleteView):
     model = Fornecedor
     template_name = 'fornecedor_apagar.html'
     success_url = reverse_lazy('fornecedores')
+    success_message = 'Fornecedor apagado com sucesso!'
