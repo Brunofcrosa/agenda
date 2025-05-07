2044b3c2c058c3f11d5ca59dddf570ca4f3f7628
Bruno Fetzer <brunofcrosa@hotmail.com>
2025-05-07 20:38:06 -0300

    Django part11

--- agendamentos/views.py
+++ agendamentos/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from .forms import AgendamentoListForm, AgendamentoModelForm
+from .models import Agendamento
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.contrib import messages
+from django.core.paginator import Paginator
+from django.contrib.messages.views import SuccessMessageMixin
+from django.db.models import Q
+class AgendamentosView(ListView):
+    model = Agendamento
+    template_name = 'agendamentos.html'
 
-# Create your views here.
+    def get_context_data(self, **kwargs):
+        context = super(AgendamentosView, self).get_context_data(**kwargs)
+        if self.request.GET:
+            form = AgendamentoListForm(self.request.GET)
+        else:
+            form = AgendamentoListForm()
+        context['form'] = form
+        return context
+
+    def get_queryset(self):
+        qs = super(AgendamentosView, self).get_queryset()
+        if self.request.GET:
+            form = AgendamentoListForm(self.request.GET)
+            if form.is_valid():
+                cliente = form.cleaned_data.get('cliente')
+                funcionario = form.cleaned_data.get('funcionario')
+                if cliente:
+                    qs = qs.filter(cliente=cliente)
+                if funcionario:
+                    qs = qs.filter(funcionario=funcionario)
+        if qs.count() > 0:
+            paginator = Paginator(qs, per_page=1)
+            listagem = paginator.get_page(self.request.GET.get('page'))
+            return listagem
+        else:
+            return messages.info(self.request, message='Não existem agendamentos cadastrados!')
+
+class AgendamentoAddView(SuccessMessageMixin, CreateView):
+    model = Agendamento
+    form_class = AgendamentoModelForm
+    template_name = 'agendamento_form.html'
+    success_url = reverse_lazy('agendamentos')
+    success_message = 'Agendamento cadastrado com sucesso!'
+
+class AgendamentoUpdateView(SuccessMessageMixin, UpdateView):
+    model = Agendamento
+    form_class = AgendamentoModelForm
+    template_name = 'agendamento_form.html'
+    success_url = reverse_lazy('agendamentos')
+    success_message = 'Agendamento alterado com sucesso!'
+
+class AgendamentoDeleteView(SuccessMessageMixin, DeleteView):
+    model = Agendamento
+    template_name = 'agendamento_apagar.html'
+    success_url = reverse_lazy('agendamentos')
+    success_message = 'Agendamento excluído com sucesso!'
